90ade0e23fec1b693306c022d90187cd8b9fab32
suyyaf <syafiqnaim8@gmail.com>
2021-12-14 16:01:23 +0800

    update v8

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -22,7 +22,7 @@ const Signup = ({ handleClick }) => {
           const token = res.data.token;
           localStorage.setItem("token", token);
 
-          window.location.href = "https://ws-client-frontend.herokuapp.com/";
+          window.location.replace = "https://ws-client-frontend.herokuapp.com/";
         } else {
           //validation
           console.log(`else`);
